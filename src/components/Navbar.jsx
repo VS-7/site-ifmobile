315d5f002478efdd03200c389cbf4d9e9a8ac095
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-22 21:15:31 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,69 @@
 import { NavLink } from "react-router-dom";
-import { FaHome, FaSignInAlt, FaUserPlus, FaUser, FaBook, FaSignOutAlt } from 'react-icons/fa';
+
 import { useAuthentication } from "../hooks/useAuthentication";
 import { useAuthValue } from "../../context/AuthContext";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    const { user } = useAuthValue();
-    const { logout } = useAuthentication();
 
-    return (
-        <nav className={styles.navbar}>
-            <ul className={styles.links_list}>
-                <li>
-                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>
-                        <FaHome /> Home
-                    </NavLink>
-                </li>
-                {!user && (
-                    <>
-                        <li>
-                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>
-                                <FaSignInAlt /> Login
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>
-                                <FaUserPlus /> Registrar
-                            </NavLink>
-                        </li>
-                    </>
-                )}
-                {user && (
-                    <>
-                        <li>
-                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : "")}>
-                                <FaBook /> Postar
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : "")}>
-                                <FaUser /> Dashboard
-                            </NavLink>
-                        </li>
-                    </>
-                )}
-                <li>
-                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>
-                        <FaBook /> Sobre
-                    </NavLink>
-                </li>
-                {user && (
+    const {user} = useAuthValue();
+    const {logout} = useAuthentication();
+  return (
+    <nav className={styles.navbar}>
+        <NavLink to="/" className={styles.brand}>
+            IF <span>Mobile</span>
+        </NavLink>
+        <ul className={styles.links_list}>
+            <li>
+                <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>
+                    Home
+                </NavLink>
+            </li>
+            {!user && (
+                <>
                     <li>
-                        <button onClick={logout}><FaSignOutAlt /> Sair</button>
+                        <NavLink to="/login" 
+                            className={({isActive}) => (isActive ? styles.active : "")}>
+                            Login
+                        </NavLink>
                     </li>
-                )}
-            </ul>
-        </nav>
-    );
+                    <li>
+                        <NavLink to="/register" 
+                            className={({isActive}) => (isActive ? styles.active : "")}>
+                            Registrar
+                        </NavLink>
+                    </li>
+                </>
+            )}
+            {user && (
+                <>
+                <li>
+                    <NavLink to="/posts/create" 
+                        className={({isActive}) => (isActive ? styles.active : "")}>
+                        Postar
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard" 
+                        className={({isActive}) => (isActive ? styles.active : "")}>
+                        Dashboard
+                    </NavLink>
+                </li>
+            </>
+            )}
+            <li>
+                <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
+                    Sobre
+                </NavLink>
+            </li>
+            {user && (
+                <li>
+                    <button onClick={logout}>Sair</button>
+                </li>
+            )}
+        </ul>
+    </nav>
+  )
 }
+
+export default Navbar
